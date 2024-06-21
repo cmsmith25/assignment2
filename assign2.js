5a17d9a8b9f2d574c59f970b74cba97974a674ca
cmsmith25 <c_smith24@mail.fhsu.edu>
2024-06-21 16:15:00 -0500

    more changes

--- assign2.js
+++ assign2.js
@@ -1,26 +1,31 @@
-let x;
-
-let y = user_Input;
-
-let userGuess = document.getElementById("userGuess").value;
 
 
 function checkGuess() {
 
-    while (x = (Math.floor(Math.random) * 10)) {
+    const randomNumber = (Math.floor(Math.random) * 10) + 1;
+    let guess;
+    
 
 	
-	if (y !==x) {
-	alert("That is not the correct number. Please try again!")
-        console.log("y");
+	while (guess !== randomNumber) {
+        guess = parseInt(prompt("Guess a number between 1 and 10:"), 10);
+        
+        if (guess === randomNumber){
+            alert("You got it!");
+        }
 
-}
+        else if (guess < randomNumber) {
+            alert("That number is too low. Please try again.");
+        }
+    
+        else if (guess> randomNumber) {
+            alert("That number is too high. Please try again.");
+        }
 
-	   else {
-           alert("You guessed the correct number! Good job!")
-           console.log("correct");
-	   break;
+        else {
+            alert("That is not a valid input. Please enter a number between 1 and 10.");
+        }
 
-}
-}
+    }
+    alert("The random number was" + randomNumber + ". Great job!");
 }
