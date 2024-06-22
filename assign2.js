62f171a8e036fef0674e3b1d77969df7a595d141
cmsmith25 <c_smith24@mail.fhsu.edu>
2024-06-22 15:11:07 -0500

    reorganizing my code

--- assign2.js
+++ assign2.js
@@ -12,20 +12,21 @@ function checkGuess() {
         
         if (guess === randomNumber){
             alert("You got it!");
-        }
+        
 
-        else if (guess < randomNumber) {
+        } else if (guess < randomNumber) {
             alert("That number is too low. Please try again.");
-        }
+        
     
-        else if (guess> randomNumber) {
+        } else if (guess > randomNumber) {
             alert("That number is too high. Please try again.");
-        }
+        
 
-        else {
+        } else {
             alert("That is not a valid input. Please enter a number between 1 and 10.");
         }
 
     }
-    alert("The random number was" + randomNumber + ". Great job!");
-}
+    }
+        alert("The random number was" + randomNumber + ". Great job!");
+
--- passwordassignment/assign2part2.js
+++ passwordassignment/assign2part2.js
@@ -1,31 +1,28 @@
+function checkPass() {
 
-
-function passCheck() {
-
-   let pass1 = prompt("Please enter a new password");
-   let pass2 = prompt("Please re-enter your password for verification");
-
-do {
-   if (pass1 === ''){
-   alert("Thank you");
-   break;
-}
-      else 
-      alert("Please enter letters only");
-      continue;
-}
-
-do {
-   if (pass2 === pass1) {
-      alert("Your passwords match!");
-      break;
+   let userPass;
+   let verifyPass;
+   
+   do {
+      userPass =(prompt("Please enter a new password"));
+      console.log("The password you entered was:" + userPass);
+         if (userPass === "");
+         alert("Thank you");
+   
+    }    while (userPass !== "");
+         alert ("That is not a valid input. Please enter a new password!");
+      
+   
+   
+   do {
+      verifyPass = prompt("Please re-enter your password for verification:");
+      console.log("You entered:" + verifyPass);
+         if (verifyPass === userPass);
+         alert("Your passwords match. Thank you!");
+         break;
+   
+   }  while (verifyPass !== userPass);
+      alert ("Your passwords do not match. Please try again!");
+   
+   
    }
-
-   }
-
-   }   while (false); {
-         alert("Your passwords do not match! Please try again!");
-         continue;
-   }
-
-
