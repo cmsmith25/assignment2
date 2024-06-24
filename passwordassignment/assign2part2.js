e94eb49b47fc9b150a70e951eaba89d4531c1b87
cmsmith25 <c_smith24@mail.fhsu.edu>
2024-06-24 12:22:42 -0500

    Adding code for assignment 2 part 3

--- passwordassignment/assign2part2.js
+++ passwordassignment/assign2part2.js
@@ -1,28 +1,14 @@
-function checkPass() {
+function passConfirm() {
 
-   let userPass;
-   let verifyPass;
-   
    do {
-      userPass =(prompt("Please enter a new password"));
-      console.log("The password you entered was:" + userPass);
-         if (userPass === "");
-         alert("Thank you");
-   
-    }    while (userPass !== "");
-         alert ("That is not a valid input. Please enter a new password!");
-      
-   
-   
-   do {
-      verifyPass = prompt("Please re-enter your password for verification:");
-      console.log("You entered:" + verifyPass);
-         if (verifyPass === userPass);
-         alert("Your passwords match. Thank you!");
-         break;
-   
-   }  while (verifyPass !== userPass);
-      alert ("Your passwords do not match. Please try again!");
-   
-   
+      x = prompt("Please create a new password");
+      y = prompt("Please re-enter your password");
+
+   } while (x !== y) {
+      alert("Your passwords do not match. Please try again!");
+
+   } if (x === y) {
+      alert("Your passwords match! Thank you.");
    }
+   }
+
